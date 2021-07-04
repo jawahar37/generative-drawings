1e7946dcb10ef806e3604dab39b001791466395f
Jawahar Pinnelli <jawahar.pinnelli@gmail.com>
2021-07-04 17:04:31 +0530

    $ out.

--- src/pages/__layout.svelte
+++ src/pages/__layout.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <svelte:head>
-  <link rel="apple-touch-icon" sizes="180x180" href="${base}/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="${base}/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="${base}/favicon-16x16.png">
-  <link rel="manifest" href="${base}/site.webmanifest">
+  <link rel="apple-touch-icon" sizes="180x180" href="{base}/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="{base}/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="{base}/favicon-16x16.png">
+  <link rel="manifest" href="{base}/site.webmanifest">
   <title>Generative Drawings</title>
 </svelte:head>
 
