0dc86c834919fe8548a650b7849e1bc385c25fda
Jawahar Pinnelli <jawahar.pinnelli@gmail.com>
2021-09-14 13:08:02 +0530

    Revert sveltekit to v1.0.0-next.147

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		paths: {
 			base: '/generative-drawings',
 		},
