05cf0984cdced28425e4112a5eec26fd4c0d8b36
Jawahar Pinnelli <jawahar.pinnelli@gmail.com>
2021-11-17 11:09:40 +0530

    Update 10-print text.

--- src/lib/pieces/10print/small.svelte
+++ src/lib/pieces/10print/small.svelte
@@ -11,8 +11,8 @@
   }}>
   <svelte:fragment slot="title">10 PRINT</svelte:fragment>>
   <svelte:fragment slot="about">
-    <p>10 PRINT is a program written in the early 80s to run on a Commodore 64 system that generates a maze with a surprisingly simple condition. It prints either a \ or a / with a 50% probability.</p>
-    <p class="code">10 PRINT CHR$ (205.5 + RND (1)); : GOTO 10</p>
+    <p>10 PRINT is a BASIC program written in the early 80s to run on a Commodore 64 system that generates a maze with a surprisingly simple condition. It prints either a \ or a / with a 50% probability.</p>
+    <p class="code">10 PRINT CHR$ (205.5 + RND (1));<br> 20 GOTO 10</p>
   </svelte:fragment>
   <svelte:fragment slot="control">
     <div class="control-item">
